99eba39ad651ca4b986f05621d8d82e1b0b6a068
salmanfxrsi <salmanxprivate@gmail.com>
2025-10-11 23:39:35 +0600

    feat: implement TypeScript basic data types with examples

---
+++ Typescript/Chapter 01/dist/data_type.js
@@ -0,0 +1,20 @@
+"use strict";
+// basic data type
+Object.defineProperty(exports, "__esModule", { value: true });
+// string
+var firstName = "Salman";
+// number
+var roll = 123;
+// boolean
+var isAdmin = true;
+// undefined
+var x = undefined;
+// null
+var y = null;
+// any
+var a;
+a = 123;
+a = "Farsi";
+// Array
+var friends = ["Alex", "Michael"];
+var eligibleRollList = [122, 123];
---
+++ Typescript/Chapter 01/src/data_type.ts
@@ -0,0 +1,28 @@
+// basic data type
+
+// string
+let firstName: string = "Salman";
+
+// number
+let roll: number = 123;
+
+// boolean
+let isAdmin: boolean = true;
+
+// undefined
+let x: undefined = undefined;
+
+// null
+let y: null = null;
+
+// any
+let a: any;
+a = 123;
+a = "Farsi";
+
+// array
+let friends: string[] = ["Alex", "Michael"];
+let eligibleRollList: number[] = [122, 123];
+
+// tuple
+let personalInfo: [string, number, boolean] = ["Farsi", 1, true];
