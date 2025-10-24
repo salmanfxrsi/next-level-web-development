37818e64e7a6125cc844f8746c68c9a6158d414d
salmanfxrsi <salmanxprivate@gmail.com>
2025-10-24 19:53:34 +0600

    feat: learn conditional types in TypeScript

---
+++ Typescript/Chapter 02/src/conditional_types.ts
@@ -0,0 +1,22 @@
+{
+  // conditional types
+
+  type a1 = null;
+  type b1 = undefined;
+
+  type x = a1 extends null ? true : false; // conditional type
+  type y = a1 extends null ? true : b1 extends undefined ? undefined : any;
+
+  type Sheikh = {
+    bike: string;
+    car: string;
+    ship: string;
+    plane: string;
+  };
+
+  type CheckVehicle<T> = T extends keyof Sheikh ? true : false;
+
+  type HasTractor = CheckVehicle<"bike">;
+
+  //
+}
