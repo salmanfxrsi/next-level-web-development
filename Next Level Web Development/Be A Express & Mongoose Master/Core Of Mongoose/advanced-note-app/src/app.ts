70f68f353f6ed5c60a3c85f81c9f113e6a1b4c92
Salman Farsi <salmanfarsi9005@gmail.com>
2026-01-16 23:27:25 +0600

    feat: add update and delete endpoints for notes and modify schema

--- Next Level Web Development/Be A Express & Mongoose Master/Core Of Mongoose/advanced-note-app/src/app.ts
+++ Next Level Web Development/Be A Express & Mongoose Master/Core Of Mongoose/advanced-note-app/src/app.ts
@@ -5,20 +5,23 @@ const app: Application = express();
 
 app.use(express.json());
 
-const noteSchema = new Schema({
-  title: { type: String, required: true, trim: true },
-  content: { type: String, default: "" },
-  category: {
-    type: String,
-    enum: ["personal", "work", "study", "other"],
-    default: "personal",
+const noteSchema = new Schema(
+  {
+    title: { type: String, required: true, trim: true },
+    content: { type: String, default: "" },
+    category: {
+      type: String,
+      enum: ["personal", "work", "study", "other"],
+      default: "personal",
+    },
+    pinned: { type: Boolean, default: false },
+    tags: {
+      label: { type: String, required: true },
+      color: { type: String, default: "gray" },
+    },
   },
-  pinned: { type: Boolean, default: false },
-  tags: {
-    label: { type: String, required: true },
-    color: { type: String, default: "gray" },
-  },
-});
+  { versionKey: false, timestamps: true }
+);
 
 const Note = model("Note", noteSchema);
 
@@ -50,6 +53,29 @@ app.post("/notes/create-note", async (req: Request, res: Response) => {
   res.status(201).json({
     success: true,
     message: "note created successfully",
+    note,
+  });
+});
+
+app.patch("/notes/noteId", async (req: Request, res: Response) => {
+  const noteId = req.params.noteId;
+  const updatedBody = req.body;
+  const note = await Note.findByIdAndUpdate(noteId, updatedBody, { new: true });
+
+  res.status(201).json({
+    success: true,
+    message: "note updated successfully",
+    note,
+  });
+});
+
+app.delete("/notes/noteId", async (req: Request, res: Response) => {
+  const noteId = req.params.noteId;
+  const note = await Note.findByIdAndDelete(noteId);
+
+  res.status(201).json({
+    success: true,
+    message: "note deleted successfully",
     note,
   });
 });
