15dcfa2e5fb2ac5aa52737105e9ff4753dd1ca27
Salman Farsi <salmanfarsi9005@gmail.com>
2026-01-17 10:48:29 +0600

    chore: explore built in validations in mongoose

--- Next Level Web Development/Be A Express & Mongoose Master/Core Of Mongoose/advanced-note-app/src/app/models/user.model.ts
+++ Next Level Web Development/Be A Express & Mongoose Master/Core Of Mongoose/advanced-note-app/src/app/models/user.model.ts
@@ -2,11 +2,29 @@ import { model, Schema } from "mongoose";
 import { IUser } from "../interfaces/user.interface";
 
 const userSchema = new Schema<IUser>({
-  firstName: { type: String, required: true, trim: true },
+  firstName: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 5,
+    maxlength: 10,
+  },
   lastName: { type: String, required: true, trim: true },
-  email: { type: String, required: true, trim: true },
+  age: { type: Number, required: true, min: 18, max: 60 },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    unique: true,
+  },
   password: { type: String, required: true },
-  role: { type: String, enum: ["admin", "user"], default: "user" },
+  role: {
+    type: String,
+    enum: ["admin", "user"],
+    default: "user",
+    lowercase: true,
+  },
 });
 
 export const User = model("User", userSchema);
